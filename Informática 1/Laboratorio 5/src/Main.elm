d2f2939e14c28f66449ac9884361a3fd6bcab5f5
Gabriel-Lemus <glemus.luigi@gmail.com>
2019-08-24 14:27:04 -0600

    Laboratorio 5

--- Informática 1/Laboratorio 5/src/Main.elm
+++ Informática 1/Laboratorio 5/src/Main.elm
@@ -1,8 +1,6 @@
 module Main exposing (esPrimo, fibonacci, nPrimos, primos)
 
 --Ejercicio 2
-
-
 checkIfPrime : Int -> Int -> Bool
 checkIfPrime n x =
     if x <= 1 then
@@ -20,7 +18,6 @@ checkIfPrime n x =
     else
         checkIfPrime (n + 1) x
 
-
 esPrimo : Int -> Bool
 esPrimo x =
     checkIfPrime 2 x
@@ -28,8 +25,6 @@ esPrimo x =
 
 
 --Ejercicio 3
-
-
 fibonacci : Int -> Int
 fibonacci x =
     case x of
@@ -45,8 +40,6 @@ fibonacci x =
 
 
 --Ejercicio 4
-
-
 primos : Int -> List Int
 primos x =
     if x <= 1 then
@@ -61,8 +54,6 @@ primos x =
 
 
 --Ejercicio 5
-
-
 count n x =
     if n == 0 then
         []
@@ -72,7 +63,6 @@ count n x =
 
     else
         x :: count (n - 1) (x + 1)
-
 
 nPrimos : Int -> List Int
 nPrimos x =
