4c9c759c002a101a9f7688c283219e31b60b813d
Gabriel-Lemus <glemus.luigi@gmail.com>
2019-08-28 21:11:42 -0600

    Laboratorio 6

--- Informática 1/Laboratorio 6/src/Main.elm
+++ Informática 1/Laboratorio 6/src/Main.elm
@@ -3,56 +3,107 @@ module Main exposing (..)
 --Ejercicio 1
 iFilter : Int -> List Int -> List Int
 iFilter n ns =
-    case (n, ns) of
-        (a, []) -> []
-        (1, cs) -> []
-        (x, d::ds) -> if modBy x d /= 0 then d :: iFilter n ds else iFilter n ds
+    case ( n, ns ) of
+        ( a, [] ) ->
+            []
+
+        ( 1, cs ) ->
+            []
+
+        ( x, d :: ds ) ->
+            if modBy x d /= 0 then
+                d :: iFilter n ds
+
+            else
+                iFilter n ds
 
 esPar : Int -> Bool
 esPar x =
-    if modBy 2 x == 0 then True
-    else False
+    if modBy 2 x == 0 then
+        True
+
+    else
+        False
 
 esImpar : Int -> Bool
-esImpar x = not(esPar x)
+esImpar x =
+    not (esPar x)
+
+
 
 --Ejercicio 2
 filter : (Int -> Bool) -> List Int -> List Int
 filter func nx =
     case nx of
-        [] -> []
-        b::bs -> if func b then b::filter func bs else filter func bs
+        [] ->
+            []
 
-head : List Int -> List Int
-head nx =
+        b :: bs ->
+            if func b then
+                b :: filter func bs
+
+            else
+                filter func bs
+
+
+invsPop : List Int -> List Int
+invsPop nx =
     case nx of
-        [] -> []
-        x::lx -> lx
+        [] ->
+            []
+
+        x :: lx ->
+            lx
 
 pop : List Int -> List Int
-pop nx = nx |> List.reverse |> head |> List.reverse
+pop nx =
+    nx |> List.reverse |> invsPop |> List.reverse
+
+
 
 --Ejercicio 3
 iZipWith nx ny =
-    case (nx, ny) of
-        ([], []) -> []
-        (a, []) -> a
-        ([], b) -> b
-        (x::lx, y::ly) -> if List.length nx == List.length ny then (x+y)::iZipWith lx ly
-            else if List.length nx >= List.length ny then iZipWith (pop nx) ny
-            else iZipWith nx (pop ny)
+    case ( nx, ny ) of
+        ( [], [] ) ->
+            []
 
-{-
-        ([], []) -> []
-        (n::ns, []) -> if ns == [] then (func n) :: [] else zipWith func ns []
-        ([], m::ms) -> if ms == [] then (func m) :: [] else zipWith func [] ms
--}
+        ( a, [] ) ->
+            a
+
+        ( [], b ) ->
+            b
+
+        ( x :: lx, y :: ly ) ->
+            if List.length nx == List.length ny then
+                (x + y) :: iZipWith lx ly
+
+            else if List.length nx > List.length ny then
+                iZipWith (pop nx) ny
+
+            else
+                iZipWith nx (pop ny)
+
+
 
 --Ejercicio 4
 zipWith : (Int -> Int -> Int) -> List Int -> List Int -> List Int
 zipWith func nx ny =
-    case (nx, ny) of
-        ([], []) -> []
-        (n::ns, []) -> (func n 0) :: zipWith func ns ny
-        ([], m::ms) -> (func 0 m) :: zipWith func nx ms
-        (x::lx, y::ly) -> (func x y) :: zipWith func lx ly
+    case ( nx, ny ) of
+        ( [], [] ) ->
+            []
+
+        ( n :: ns, [] ) ->
+            func n 0 :: zipWith func ns ny
+
+        ( [], m :: ms ) ->
+            func 0 m :: zipWith func nx ms
+
+        ( x :: lx, y :: ly ) ->
+            if List.length nx == List.length ny then
+                func x y :: zipWith func lx ly
+
+            else if List.length nx > List.length ny then
+                zipWith func (pop nx) ny
+
+            else
+                zipWith func nx (pop ny)
