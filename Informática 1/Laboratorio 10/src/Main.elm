2e1accf42d58e18fc73a7253a172cfb40e206c92
Gabriel-Lemus <glemus.luigi@gmail.com>
2019-10-14 18:03:19 -0600

    Laboratorio 10

--- Informática 1/Laboratorio 10/src/Main.elm
+++ Informática 1/Laboratorio 10/src/Main.elm
@@ -6,9 +6,10 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
 type alias Model =
-    { number : String,
-    listOfResults : List String,
-    result : String }
+    { number : String
+    , expression : List String
+    , result : String
+    }
 
 
 type Msg
@@ -18,8 +19,10 @@ type Msg
     | Resetear
     | Calcular
 
+
 strToInt : String -> Int
-strToInt num = Maybe.withDefault 0 (String.toInt num)
+strToInt num =
+    Maybe.withDefault 0 (String.toInt num)
 
 listToString : List String -> String
 listToString list =
@@ -29,17 +32,24 @@ listToString list =
 
 isNotOperator : String -> Bool
 isNotOperator str =
-    case str of
-        x -> if x == "+" || x == "*" then False else True 
+    if str == "+" || str == "x" then False else True
 
 combineExpressionAux : List String -> List String
 combineExpressionAux list =
     case list of
         [] -> []
-        [x] -> [x]
-        x :: y :: xys -> if isNotOperator x && isNotOperator y then (x ++ y) :: combineExpression xys
-                         else if not(isNotOperator x) then x :: combineExpression (y :: xys)
-                         else ([x] ++ [y]) ++ combineExpression xys
+
+        [x] ->[x]
+
+        x :: y :: xys ->
+            if isNotOperator x && isNotOperator y then
+                (x ++ y) :: combineExpression xys
+
+            else if not (isNotOperator x) then
+                x :: combineExpression (y :: xys)
+
+            else
+                ([x] ++ [y]) ++ combineExpression xys
 
 combineExpression : List String -> List String
 combineExpression list =
@@ -54,72 +64,113 @@ firstElement list =
 restOfList : List String -> List String
 restOfList list =
     case list of
-        [] -> [""]
+        [] ->[ "" ]
         x :: xs -> xs
 
 evaluateMultiplication : List String -> List String
 evaluateMultiplication list =
     case list of
-        [] -> ["0"]
+        [] -> [ "0" ]
+
         [x] -> [x]
-        x :: y :: xys -> if y == "*" then evaluateMultiplication ((String.fromInt ((strToInt x) * (strToInt (firstElement xys)))) :: (restOfList xys))
-                         else x :: y :: evaluateMultiplication xys
+
+        x :: y :: xys ->
+            if y == "x" then
+                evaluateMultiplication (String.fromInt (strToInt x * strToInt (firstElement xys)) :: restOfList xys)
+
+            else
+                x :: y :: evaluateMultiplication xys
 
 evaluateAddition : List String -> List String
 evaluateAddition list =
     case list of
-        [] -> ["0"]
+        [] -> [ "0" ]
+
         [x] -> [x]
-        x :: y :: xys -> if y == "+" then evaluateAddition ((String.fromInt ((strToInt x) + (strToInt (firstElement xys)))) :: (restOfList xys))
-                         else x :: y :: evaluateAddition xys
+
+        x :: y :: xys ->
+            if y == "+" then
+                evaluateAddition (String.fromInt (strToInt x + strToInt (firstElement xys)) :: restOfList xys)
+
+            else
+                x :: y :: evaluateAddition xys
 
 evaluate : List String -> String
 evaluate list =
     combineExpression list |> evaluateMultiplication |> evaluateAddition |> firstElement
 
+
 init : Model
 init =
-    Model "0" ["0"] "0"
+    Model "0" [ "0" ] "0"
 
--- number, listOfResults, result
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Concat x -> if model.number == "0" then Model x [x] "0" 
-            else if model.number == model.result then  Model x [x] "0"
-            else Model (model.number ++ x) (model.listOfResults ++ [x]) "0"
-        Sumar -> Model (model.number ++ "+") (model.listOfResults ++ ["+"]) "0"
-        Multiplicar -> Model (model.number ++ "*") (model.listOfResults ++ ["*"]) "0"
-        Resetear -> Model "0" ["0"] "0" 
-        Calcular -> Model (evaluate model.listOfResults) (model.listOfResults) (evaluate model.listOfResults)
+        Concat x ->
+            if model.number == "0" then
+                Model x [ x ] "0"
+
+            else if model.number == model.result then
+                Model x [ x ] (model.result)
+
+            else
+                Model (model.number ++ x) (model.expression ++ [ x ]) (model.result)
+
+        Sumar ->
+            Model (model.number ++ "+") (model.expression ++ [ "+" ]) (model.result)
+
+        Multiplicar ->
+            Model (model.number ++ "x") (model.expression ++ [ "x" ]) (model.result)
+
+        Resetear ->
+            Model "0" [ "0" ] "0"
+
+        Calcular ->
+            Model (evaluate model.expression) model.expression (evaluate model.expression)
+
 
 view : Model -> Html Msg
 view model =
-    div [] [
-        button [ onClick (Concat "9") ] [ text "9" ],
-        button [ onClick (Concat "8") ] [ text "8" ],
-        button [ onClick (Concat "7") ] [ text "7" ],
-        button [ onClick (Concat "6") ] [ text "6" ],
-        button [ onClick (Concat "5") ] [ text "5" ],
-        button [ onClick (Concat "4") ] [ text "4" ],
-        button [ onClick (Concat "3") ] [ text "3" ],
-        button [ onClick (Concat "2") ] [ text "2" ],
-        button [ onClick (Concat "1") ] [ text "1" ],
-        button [ onClick (Concat "0") ] [ text "0" ],
-        button [ onClick Sumar ] [ text "+" ],
-        button [ onClick Multiplicar ] [ text "*" ],
-        button [ onClick Calcular ] [ text "=" ],
-        button [ onClick Resetear ] [ text "Resetear" ],
-        div [] [ text (listToString model.listOfResults) ],
-        div [] [ text "=" ],
-        div [] [ text model.result ]
+    div [ Html.Attributes.class "main-div" ] [
+        div [ Html.Attributes.class "calculator" ]
+        [ 
+            div [ Html.Attributes.class "resultado" ] [
+                div [] [ text (listToString model.expression) ]
+                , div [] [ text model.result ]
+            ],
+            div [ Html.Attributes.class "buttons" ] [
+                div [ Html.Attributes.class "7-9x" ] [
+                    button [ onClick (Concat "7"), Html.Attributes.class "numButton" ] [ text "7" ]
+                    , button [ onClick (Concat "8"), Html.Attributes.class "numButton" ] [ text "8" ]
+                    , button [ onClick (Concat "9"), Html.Attributes.class "numButton" ] [ text "9" ]
+                    , button [ onClick Multiplicar, Html.Attributes.class "opButton" ] [ text "x" ]
+                ],
+                div [ Html.Attributes.class "4-6+" ] [
+                    button [ onClick (Concat "4"), Html.Attributes.class "numButton" ] [ text "4" ]
+                    , button [ onClick (Concat "5"), Html.Attributes.class "numButton" ] [ text "5" ]
+                    , button [ onClick (Concat "6"), Html.Attributes.class "numButton" ] [ text "6" ]
+                    , button [ onClick Sumar, Html.Attributes.class "opButton" ] [ text "+" ]
+                ],
+                div [ Html.Attributes.class "1-3=" ] [
+                    button [ onClick (Concat "1"), Html.Attributes.class "numButton" ] [ text "1" ]
+                    , button [ onClick (Concat "2"), Html.Attributes.class "numButton" ] [ text "2" ]
+                    , button [ onClick (Concat "3"), Html.Attributes.class "numButton" ] [ text "3" ]
+                    , button [ onClick Calcular, Html.Attributes.class "opButton" ] [ text "=" ]
+                ],
+                div [ Html.Attributes.class "0_reset" ] [
+                    button [ onClick (Concat "0"), Html.Attributes.class "numButton" ] [ text "0" ]
+                    , button [ onClick Resetear, Html.Attributes.class "resetButton" ] [ text "AC" ]
+                ]
+            ]
+        ]
     ]
+
 
 main =
     Browser.sandbox
-    {
-        init = init,
-        view = view,
-        update = update
-    }
+        { init = init
+        , view = view
+        , update = update
+        }
